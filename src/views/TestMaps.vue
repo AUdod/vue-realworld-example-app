39c8dbb42364e363abd1c21bed0571c75c726cb1
Антон Удод <udod@aigeo.ru>
2019-10-02 19:37:35 +0700

    files

---
+++ src/views/TestMaps.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div id="yaMap" style="height: 400px; background-color: yellow"></div>
+    <div id="googleMaps" style="height: 400px; background-color: yellow"></div>
+  </div>
+</template>
+
+<script>
+import ymaps from "ymaps";
+/* import 'google-maps' as gmaps; */
+
+/* gmaps.load(function(google) {
+  new google.maps.Map(el, options);
+});
+ */
+ymaps
+  .load()
+  .then(maps => {
+    const map = new maps.Map("yaMap", {
+      center: [-8.369326, 115.166023],
+      zoom: 7
+    });
+    console.log(map);
+  })
+  .catch(error => console.log("Failed to load Yandex Maps", error));
+
+/* gmaps.createClient({
+    key: 'AIzaSyBFlJvRnzuIJo_Et-c48xss9hcUeS_eUZk'
+}) */
+
+export default {
+  name: "test-maps"
+};
+</script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1>This is test, chmoha :^*</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test-view"
+};
+</script>
